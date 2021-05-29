2017bc3f2046c5424d043fa33566d50eebd30dae
Varun Gollapalli <gvarun1999@gmail.com>
2021-05-29 08:55:24 +0530

    Create CirclularArrayRotation.java

---
+++ Problem Solving/CirclularArrayRotation.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'circularArrayRotation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY queries
+     */
+
+    public static List<Integer> circularArrayRotation(List<Integer> a, int k, List<Integer> queries) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i =0;i<a.size();i++)
+        {
+            list.add(a.get(i));
+        }
+        
+        int i =1;
+        int n = list.size()-1;
+        while(i<=k)
+        {
+            int last = list.remove(n);
+            list.add(0,last);
+            
+            i++;
+        }
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        for(int j = 0;j<queries.size();j++)
+        {
+            ans.add(list.get(queries.get(j)));
+        }
+        
+        return ans;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int q = Integer.parseInt(firstMultipleInput[2]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> queries = IntStream.range(0, q).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.circularArrayRotation(a, k, queries);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
